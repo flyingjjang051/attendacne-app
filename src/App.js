47ac2a417623449a4c44f6d8d06966ddb391389f
flyingjjang051 <flyjjang051@gmail.com>
2022-09-13 10:08:41 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./css/layout.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="container contents">
+        <p className="total">TOTAL : 0</p>
+      </div>
+      <div className="inputBox">
+        <input type="text" placeholder="이름을 입력하세요." />
+        <button className="btn">ADD</button>
+      </div>
+      <div className="studentList"></div>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
